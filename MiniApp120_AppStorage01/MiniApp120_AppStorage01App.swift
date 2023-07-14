b93c1fc21c9de13341b2a3e0ff30131e575c9c70
Kota <maedakoutaa@gmail.com>
2023-07-14 10:12:23 +0900

    Initial Commit

---
+++ MiniApp120_AppStorage01/MiniApp120_AppStorage01App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp120_AppStorage01App.swift
+//  MiniApp120_AppStorage01
+//
+//  Created by 前田航汰 on 2023/07/14.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp120_AppStorage01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
